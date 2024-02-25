2f070faa193f1fd55124057cc30ec7397eeecc0a
jurgi5 <jurgis.satunas@gmail.com>
2024-02-25 18:19:47 +0200

    orange script works

--- Assets/Scripts/Orange.cs
+++ Assets/Scripts/Orange.cs
@@ -6,26 +6,26 @@ public class Orange : MonoBehaviour
 {
     public int scoreValue = 0;
     public GameObject coconutPrefab;
-    private int counter = 0;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Orange"))
         {
-            counter++;
-            Vector3 collisionPosition = collision.contacts[0].point;    
+            int collisionCount = PlayerPrefs.GetInt("CollisionCount", 0);
+            collisionCount++;
+            PlayerPrefs.SetInt("CollisionCount", collisionCount);
+
+            PlayerPrefs.Save();
+
+            Vector3 collisionPosition = collision.contacts[0].point;
+            
             Destroy(collision.gameObject);
             Destroy(gameObject);
-            if (counter % 2 == 0)
+
+            if (collisionCount % 2 == 0)
             {
                 Instantiate(coconutPrefab, collisionPosition, Quaternion.identity);
             }
         }
     }
-
-    private void Update()
-    {
-        print(counter);
-    }
-
 }
